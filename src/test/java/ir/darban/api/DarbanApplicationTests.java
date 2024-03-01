6d98355dfceb30a8a4c9b499e8b0b364a3294c3e
moseyza <Moseyza@gmail.con>
2024-03-01 12:43:51 +0330

    first commit

---
+++ src/test/java/ir/darban/api/DarbanApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.darban.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarbanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
